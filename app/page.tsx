601841bd4ca970c0a1e2b0263b40fa0f37f99ac8
Yash Patel <yrp.baps@gmail.com>
2026-01-11 04:37:43 +0530

    landing page redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      router.push("/dashboard");
+    }
+  }, [router]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <header className="p-4 flex justify-between items-center max-w-7xl mx-auto w-full">
